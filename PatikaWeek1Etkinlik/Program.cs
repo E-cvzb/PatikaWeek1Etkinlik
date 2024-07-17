6324a831fd4fad3f84da03b8ab4b9ff4e23cc7c4
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-07-18 01:45:09 +0300

    Proje dosyası ekle.

---
+++ PatikaWeek1Etkinlik/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("lütfen sayı giriniz");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>=10)
+{
+    if (number % 2 ==0)
+    {
+        Console.WriteLine("girilen sayı 10 dan büyüktür");
+        Console.WriteLine("giren sayı çifttir");
+    }
+    else if (number==10)
+    {
+        Console.WriteLine("giren sayı 10 a eşittir");
+        Console.WriteLine("giren sayı çifttir");
+    }
+    
+    
+    else
+    {
+
+        Console.WriteLine("giren sayı 10 dan büyüktür");
+        Console.WriteLine("giren sayı tektir");
+    }
+
+
+
+}
+else if (number<10 && number>0)
+{
+    if (number%2==0)
+    {
+        Console.WriteLine("giren sayı 10 dan küçüktür");
+        Console.WriteLine("giren sayı çifttir");
+    
+    
+
+    }
+    else
+    {
+        Console.WriteLine("giren sayı 10 dan küçüktür");
+        Console.WriteLine("giren sayı tektir");
+
+    }
+
+}
+else if (number==0)
+{
+    Console.WriteLine("giren sayı 10 dan küçük ve 0 a eşittir");
+    Console.WriteLine("giren sayı çifttir");
+}
+
+else
+{
+    if (number%2 ==0)
+    {
+        Console.WriteLine("giren sayı 10 dan küçük ve nagatiftir.");
+        Console.WriteLine("giren sayı çiftit");
+    }
+    else
+    {
+        Console.WriteLine("girilen sayı 10 dan küçük ve negatifir.");
+        Console.WriteLine("girilen sayı tektir");
+    }
+}
